8e2db9d663db09f50dbd6bb6a8666e9b610d2d72
joelsonpaiva <joelsonp.dev@gmail.com>
2020-06-07 21:17:00 -0300

    version 2.0

--- js/main.js
+++ js/main.js
@@ -2,28 +2,29 @@ function Load() {
     var msg = window.document.getElementById('msg');
     var img = window.document.getElementById('image');
     var data = new Date();
-    var hour = data.getHours();
+    var hour = 22
+    //var hour = data.getHours();
     msg.innerHTML = 'Agora são ${hour} horas.';
     
     if (hour >=0 && hour<12 ){
         //bom dia
         img.src = 'img/manha.png';
         document.body.style.background = '#e2cd9f';
-        msg.innerHTML = 'Good morning is ' + hour + ' am.';
+        msg.innerHTML = "Good morning, it's " + hour + ' hrs';
     } else if (hour>= 12 && hour <18){
         //boa tarde
         img.src = 'img/tarde.png';
         document.body.style.background = '#b9846f';
-        msg.innerHTML = 'Good afternoon is ' + hour + ' pm.';
+        msg.innerHTML = "Good afternoon, it's " + hour + ' hrs';
     } else if (hour >=18 && hour <21) {
         //boa noite
         img.src = 'img/noite.png';
         document.body.style.background = '#515154';
-        msg.innerHTML = 'Good evening is ' + hour + ' hours.';
+        msg.innerHTML = "Good evening, it's " + hour + ' hrs';
     } else {
         img.src = 'img/noite.png';
         document.body.style.background = '#515154';
-        msg.innerHTML = 'Good night is ' + hour + ' hours.';
+        msg.innerHTML = "Good night, it's " + hour + ' hrs';
 
     }
 }
